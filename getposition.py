06f4dc075193ffb70297b215f041b4e5d7c62449
Richard Williamson <richard.williamson@waterlinked.com>
2021-10-22 12:42:04 +0200

    Add depth to global position in getposition.py
    In addition, we add an option to getposition.py to allow for the
    acoustic position to be calculated relative to the (mid-point of the
    base of) an antenna.

--- getposition.py
+++ getposition.py
@@ -1,10 +1,9 @@
 """
 Get position from Water Linked Underwater GPS
 """
-import requests
 import argparse
 import json
-
+import requests
 
 def get_data(url):
     try:
@@ -19,33 +18,68 @@ def get_data(url):
 
     return r.json()
 
+def get_antenna_position(base_url):
+    return get_data("{}/api/v1/config/antenna".format(base_url))
+
 def get_acoustic_position(base_url):
     return get_data("{}/api/v1/position/acoustic/filtered".format(base_url))
 
-
-def get_global_position(base_url):
+def get_global_position(base_url, acoustic_depth = None):
     return get_data("{}/api/v1/position/global".format(base_url))
-
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-u', '--url', help='Base URL to use', type=str, default='http://demo.waterlinked.com')
+    parser.add_argument(
+        "-u",
+        "--url",
+        help = "Base URL to use",
+        type = str,
+        default = "https://demo.waterlinked.com")
+    parser.add_argument(
+        "-a",
+        "--antenna",
+        action = "store_true",
+        help = (
+            "Use mid-point of base of antenna as origin for the acoustic " +
+            "position. Default origin is the point at sea level directly " +
+            "below/above that which the positions of the receivers/antenna " +
+            "are defined with respect to"))
     args = parser.parse_args()
 
     base_url = args.url
     print("Using base_url: %s" % args.url)
 
-    data = get_acoustic_position(base_url)
-    if data:
-        #print(data)
-        print("Current acoustic position {},{},{}".format(data["x"], data["y"], data["z"]))
+    acoustic_position = get_acoustic_position(base_url)
+    antenna_position = None
+    if args.antenna:
+        antenna_position = get_antenna_position(base_url)
+    depth = None
+    if acoustic_position:
+        if antenna_position:
+            print(acoustic_position)
+            print(antenna_position)
+            print("Current acoustic position relative to antenna. X: {}, Y: {}, Z: {}".format(
+                acoustic_position["x"] - antenna_position["x"],
+                acoustic_position["y"] - antenna_position["y"],
+                acoustic_position["z"] - antenna_position["depth"]))
+        else:
+            print("Current acoustic position. X: {}, Y: {}, Z: {}".format(
+                acoustic_position["x"],
+                acoustic_position["y"],
+                acoustic_position["z"]))
+        depth = acoustic_position["z"]
 
-    pos = get_global_position(base_url)
-    if pos:
-        #print(pos)
-        print("Current global position lat:{} lon:{}".format(pos["lat"], pos["lon"]))
-
-
+    global_position = get_global_position(base_url)
+    if global_position:
+        if depth:
+            print("Current global position. Latitude: {}, Longitude: {}, Depth: {}".format(
+                global_position["lat"],
+                global_position["lon"],
+                depth))
+        else:
+            print("Current global position latitude :{} longitude :{}".format(
+                global_position["lat"],
+                global_position["lon"]))
 
 if __name__ == "__main__":
     main()
